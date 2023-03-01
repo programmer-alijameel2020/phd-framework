2e1b3c73b097d8e8f5fcdea50be78109f666367a
programmer-alijameel2020 <68697982+programmer-alijameel2020@users.noreply.github.com>
2023-03-01 10:43:44 +0300

    AutoEncoder completion with metrics

--- Framework/NeuroAE.py
+++ Framework/NeuroAE.py
@@ -109,9 +109,9 @@ class Autoencoder(Model):
     def run_autoEncoder(self, dataset, epochs, batch_size, stopping_patience, generation):
         df = pd.read_csv(dataset, sep='  ', header=None, engine='python')
         # df = pd.read_csv('storage/dataset/test.txt', sep='  ', header=None, engine='python')
-        print(df.head())
+        # print(df.head())
         df.columns
-        print(df.describe())
+        # print(df.describe())
         # splitting into train test data
         train_data, test_data, train_labels, test_labels = train_test_split(df.values, df.values[:, 0:1], test_size=0.2,
                                                                             random_state=111)
@@ -129,7 +129,7 @@ class Autoencoder(Model):
         normal_train_data = pd.DataFrame(train_data_scaled).add_prefix('c').query('c0 == 0').values[:, 1:]
         anomaly_train_data = pd.DataFrame(train_data_scaled).add_prefix('c').query('c0 > 0').values[:, 1:]
 
-        print("Anomaly training data: ", anomaly_train_data)
+        # print("Anomaly training data: ", anomaly_train_data)
 
         # Making pandas dataframe for the normal and anomaly test data points
         normal_test_data = pd.DataFrame(test_data_scaled).add_prefix('c').query('c0 == 0').values[:, 1:]
@@ -146,6 +146,7 @@ class Autoencoder(Model):
         # Compiling the model
         model.compile(optimizer='adam',
                       loss='mae')
+        csv_logger = CSVLogger('metrics.csv', append=True)
 
         # Training the model
         history = model.fit(normal_train_data, normal_train_data,
@@ -153,49 +154,49 @@ class Autoencoder(Model):
                             batch_size=batch_size,
                             validation_data=(train_data_scaled[:, 1:], train_data_scaled[:, 1:]),
                             shuffle=True,
-                            callbacks=[early_stopping])
+                            callbacks=[early_stopping, csv_logger])
 
         # predictions for normal test data points
         encoder_out = model.encoder(normal_test_data).numpy()
         decoder_out = model.decoder(encoder_out).numpy()
 
-        print("Test data:", encoder_out)
-        print("Predictions:", decoder_out)
-        print(decoder_out.shape)
-        print(normal_test_data[0], 'b')
-        print(decoder_out[0], 'r')
+        # print("Test data:", encoder_out)
+        # print("Predictions:", decoder_out)
+        # print(decoder_out.shape)
+        # print(normal_test_data[0], 'b')
+        # print(decoder_out[0], 'r')
 
         # predictions for anomaly test data points
         encoder_out_a = model.encoder(anomaly_test_data).numpy()
         decoder_out_a = model.decoder(encoder_out_a).numpy()
 
-        print("Anomaly training data", anomaly_train_data[0], 'b')
-        print("Decoder out: ", decoder_out_a[0], 'r')
+        # print("Anomaly training data", anomaly_train_data[0], 'b')
+        # print("Decoder out: ", decoder_out_a[0], 'r')
 
         # reconstruction loss for normal test data
         reconstructions = model.predict(normal_test_data)
         train_loss = tf.keras.losses.mae(reconstructions, normal_test_data)
 
         # Plotting histogram for recontruction loss for normal test data
-        print("Training loss", train_loss)
-        print("Mean: ", np.mean(train_loss))
-        print("Standard deviation: ", np.std(train_loss))
+        # print("Training loss", train_loss)
+        # print("Mean: ", np.mean(train_loss))
+        # print("Standard deviation: ", np.std(train_loss))
 
         # reconstruction loss for anomaly test data
         reconstructions_a = model.predict(anomaly_test_data)
         train_loss_a = tf.keras.losses.mae(reconstructions_a, anomaly_test_data)
 
         # Plotting histogram for reconstruction loss for anomaly test data
-        print("Reconstructed training loss: ", train_loss_a)
-        print("Mean: ", np.mean(train_loss_a))
-        print("Standard deviation: ", np.std(train_loss_a))
+        # print("Reconstructed training loss: ", train_loss_a)
+        # print("Mean: ", np.mean(train_loss_a))
+        # print("Standard deviation: ", np.std(train_loss_a))
 
         # setting threshold
         threshold = np.mean(train_loss) + 2 * np.std(train_loss)
-        print("Threshold: ", threshold)
+        # print("Threshold: ", threshold)
 
-        print("normal_test_data: ", normal_test_data)
-        print("Predictions: ", reconstructions)
+        # print("normal_test_data: ", normal_test_data)
+        # print("Predictions: ", reconstructions)
 
         # Plotting the normal and anomaly losses with the threshold
         plt.hist(train_loss, bins=50, density=True, label="Normal (train data loss)", alpha=.6, color="green")
